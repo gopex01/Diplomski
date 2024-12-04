0f1deec0c13ef3654e8f0afe83f2855e444cd411
Predrag <predragzivkovic@elfak.rs>
2024-12-04 18:27:01 +0100

    kamere

--- Client/Front/src/app/personal-travel-view/personal-travel-view.component.ts
+++ Client/Front/src/app/personal-travel-view/personal-travel-view.component.ts
@@ -6,6 +6,23 @@ import { TravelService } from '../services/travel.service';
 import * as L from 'leaflet';
 import 'leaflet-routing-machine';
 import 'leaflet-control-geocoder';
+import { CameraPresevoComponent } from '../camera-presevo/camera-presevo.component';
+import { CameraGradinaComponent } from '../camera-gradina/camera-gradina.component';
+import { CameraVrskaCukaComponent } from '../camera-vrska-cuka/camera-vrska-cuka.component';
+import { CameraTrbusnicaComponent } from '../camera-trbusnica/camera-trbusnica.component';
+import { CameraRacaComponent } from '../camera-raca/camera-raca.component';
+import { CameraZvornikComponent } from '../camera-zvornik/camera-zvornik.component';
+import { CameraKotromanComponent } from '../camera-kotroman/camera-kotroman.component';
+import { CameraVatinComponent } from '../camera-vatin/camera-vatin.component';
+import { CameraSidComponent } from '../camera-sid/camera-sid.component';
+import { CameraBatrovciComponent } from '../camera-batrovci/camera-batrovci.component';
+import { CameraGostunComponent } from '../camera-gostun/camera-gostun.component';
+import { CameraJabukaComponent } from '../camera-jabuka/camera-jabuka.component';
+import { CameraHorgosComponent } from '../camera-horgos/camera-horgos.component';
+import { CameraKelebijaComponent } from '../camera-kelebija/camera-kelebija.component';
+import { CameraCalaComponent } from '../camera-cala/camera-cala.component';
+import * as turf from '@turf/turf';
+import { Dialog } from '@angular/cdk/dialog';
 @Component({
   selector: 'app-personal-travel-view',
   templateUrl: './personal-travel-view.component.html',
@@ -13,29 +30,51 @@ import 'leaflet-control-geocoder';
 })
 export class PersonalTravelViewComponent implements OnInit{
 
-  startPoint:string;
-  endPoint:string;
+  startPoint:string;//inicijalizuje se kroz query iz search
+  endPoint:string;//inicijalizuje se kroz query iz search
   map?:L.Map;
-  selectedOption: any='None';
-  proposedPOIS:any[]=[];
-  proposedRestaurants:any[]=[];
-  constructor(private route:ActivatedRoute,private geocodingService:GeocodingService,private http:HttpClient,private travelService:TravelService)
+  selectedOption: any='None';//za radio buttoni da se izabere kakav prikaz zelimo na mapi
+  proposedPOIS:any[]=[];//preporucene pumpe za pauzu
+  proposedRestaurants:any[]=[];//preporuceni restorani za pauzu
+
+  bulgariaGeoJson:any;
+  bosniaGeoJson:any;
+  croatiaGeoJson:any;
+  hungaryGeoJson:any;
+  macedoniaGeoJson:any;
+  montenegroGeoJson:any;
+  rumunyGeoJson:any;
+
+  isRouteInBulgaria:boolean=false;
+  isRouteInBosnia:boolean=false;
+  isRouteInCroatia:boolean=false;
+  isRouteInHungary:boolean=false;
+  isRouteInMacedonia:boolean=false;
+  isRouteInMontenegro:boolean=false;
+  isRouteInRumuny:boolean=false;
+
+  constructor(private geocodingService:GeocodingService,
+    private http:HttpClient
+    ,private route: ActivatedRoute
+    ,private travelService:TravelService,
+    private dialog:Dialog)
   {
     this.startPoint='';
     this.endPoint='';
+    
   }
-
+ 
   async calculateRoute(option:any)
   {
     try {
       const startCoords = await this.geocodingService.getCoordinates(this.startPoint).toPromise();
       const endCoords = await this.geocodingService.getCoordinates(this.endPoint).toPromise();
+      console.log('start cords odgovor ',startCoords);
 
       const startLatLng = L.latLng(startCoords!.results[0].geometry.lat, startCoords!.results[0].geometry.lng);
       const endLatLng = L.latLng(endCoords!.results[0].geometry.lat, endCoords!.results[0].geometry.lng);
 
       this.addRoute(startLatLng, endLatLng,option);
-     
     } catch (error) {
       console.error('Greška prilikom dobijanja koordinata:', error);
     }
@@ -60,20 +99,34 @@ export class PersonalTravelViewComponent implements OnInit{
       show:false,
     }).addTo(this.map!);
 
-    routingControl.on('routesfound',(e:any)=>{
+    routingControl.on('routesfound', async (e:any)=>{
       const routes=e.routes;
       const summary=routes[0].summary;
       const routeCoords=routes[0].coordinates;
-      console.log("broj tacaka na ruti",routeCoords);
-      console.log(`Dužina rute: ${summary.totalDistance / 1000} km`);
-      console.log(`Procenjeno vreme putovanja: ${summary.totalTime / 60} minuta`);
+
+      await this.loadBulgariaJson();
+      await this.loadBosniaJson();
+      await this.loadCroatiaJson();
+      await this.loadHungaryJson();
+      await this.loadMacedoniaJson();
+      await this.loadMontenegroJson();
+      await this.loadRumunyJson();
+
+      this.isRouteInBulgaria=this.checkRouteInCountry(routeCoords,this.bulgariaGeoJson);
+      this.isRouteInBosnia=this.checkRouteInCountry(routeCoords,this.bosniaGeoJson);
+      this.isRouteInCroatia=this.checkRouteInCountry(routeCoords,this.croatiaGeoJson);
+      this.isRouteInHungary=this.checkRouteInCountry(routeCoords,this.hungaryGeoJson);
+      this.isRouteInMacedonia=this.checkRouteInCountry(routeCoords,this.macedoniaGeoJson);
+      this.isRouteInMontenegro=this.checkRouteInCountry(routeCoords,this.montenegroGeoJson);
+      this.isRouteInRumuny=this.checkRouteInCountry(routeCoords,this.rumunyGeoJson);
+      
       const totalTimeInMinutes = summary.totalTime / 60;
       const totalTimeInSeconds=summary.totalTime;
       const hours = Math.floor(totalTimeInMinutes / 60);
       const minutes = Math.floor(totalTimeInMinutes % 60);
       const popupContent = `
-      <b>Dužina rute:</b> ${(summary.totalDistance / 1000).toFixed(2)} km<br>
-      <b>Procenjeno vreme putovanja:</b> ${hours} sati i ${minutes} minuta`;
+      <b>Travel distance:</b> ${(summary.totalDistance / 1000).toFixed(2)} km<br>
+      <b>Travel time:</b> ${hours} hours and ${minutes} minutes`;
       L.marker(start)
       .addTo(this.map!)
       .bindPopup(popupContent)
@@ -110,9 +163,9 @@ export class PersonalTravelViewComponent implements OnInit{
     
         const customIcon=L.icon({
           iconUrl:'../../assets/images/gas-station.png',
-          iconSize: [12, 12], // Prilagodi veličinu po potrebi
-          iconAnchor: [16, 32], // Podesi tačku sidrišta ako je potrebno
-          popupAnchor: [0, -32] // Podesi tačku za prikaz popup-a
+          iconSize: [12, 12], 
+          iconAnchor: [16, 32], 
+          popupAnchor: [0, -32] 
         });
         const filteredPOIs = data.elements.filter((element: any) => {
           if (element.type === 'node') {
@@ -127,8 +180,8 @@ export class PersonalTravelViewComponent implements OnInit{
     
         filteredPOIs.forEach((element: any) => {
           const fuelStation = L.marker([element.lat, element.lon],{icon:customIcon}).addTo(this.map!);
-          fuelStation.bindPopup('Benzinska pumpa');
-          fuelStation.on('click',()=>{
+          fuelStation.bindPopup('Petrol Station');
+          fuelStation.on('click', () => {
             const stationCoords = L.latLng(element.lat, element.lon);
     
             // Kreiraj novu rutu do benzinske pumpe
@@ -141,8 +194,8 @@ export class PersonalTravelViewComponent implements OnInit{
             }).addTo(this.map!);
           });
         });
-    
-        console.log('Filtrirani POI:', filteredPOIs);
+        
+       // console.log('Filtrirani POI:', filteredPOIs);
       });
   }
   async getPOIsOnRouteV2(routeCoords: L.LatLng[], display: boolean,iteration:number): Promise<void> {
@@ -169,18 +222,18 @@ export class PersonalTravelViewComponent implements OnInit{
               return false;
             });
             filteredPOIs.forEach((poi: any) => {
-              poi.iteration = iteration; // Dodavanje atributa 'iteration'
+              poi.iteration = iteration; // Dodavanje atributa 'iteration' da bi znali koji je redni broj pauze
             });
-            console.log('Ove pumpe sam našao na ruti', filteredPOIs);
+        
   
             if (display) {
               filteredPOIs.forEach((element: any) => {
                 const fuelStation = L.marker([element.lat, element.lon], { icon: customIcon }).addTo(this.map!);
-                fuelStation.bindPopup('Benzinska pumpa');
+                fuelStation.bindPopup('Petrol station');
               });
             } else {
               if (filteredPOIs.length > 3) {
-                let pomocni = filteredPOIs.slice(0, 5);
+                let pomocni = filteredPOIs.slice(0, 3);
                 pomocni.forEach((x: any) => {
                   this.proposedPOIS.push(x);
                 });
@@ -194,7 +247,7 @@ export class PersonalTravelViewComponent implements OnInit{
                 (obj, index, self) =>
                   index === self.findIndex(o => o.lat === obj.lat && o.lon === obj.lon)
               );
-              console.log('Proposed', this.proposedPOIS);
+              //console.log('Proposed pumpe', this.proposedPOIS);
               
             }
             resolve(); // Signalizira završetak
@@ -203,15 +256,16 @@ export class PersonalTravelViewComponent implements OnInit{
         });
     });
   }
+  
   showAccmmodationsOnRoute(routeCoords:L.LatLng[]){
     this.geocodingService.getAccomodationsOnRoute(routeCoords[0],routeCoords[routeCoords.length-1])
     .subscribe((data:any)=>{
       const radius=5000;
       const accomodationIcon=L.icon({
         iconUrl: '../../assets/images/hotel.png', // Putanja do ikone prenoćišta
-        iconSize: [16, 16], // Prilagodi veličinu po potrebi
-        iconAnchor: [8, 8], // Podesi tačku sidrišta
-        popupAnchor: [0, -8] // Podesi tačku za prikaz popup-a
+        iconSize: [16, 16], 
+        iconAnchor: [8, 8], 
+        popupAnchor: [0, -8] 
       });
       
       const filteredAccommodations=data.elements.filter((element:any)=>{
@@ -228,21 +282,21 @@ export class PersonalTravelViewComponent implements OnInit{
 
       filteredAccommodations.forEach((element:any)=>{
         const accommodationMarker=L.marker([element.lat,element.lon],{icon:accomodationIcon}).addTo(this.map!);
-        accommodationMarker.bindPopup('prenociste');
+        accommodationMarker.bindPopup('accommodation');
         accommodationMarker.on('click',()=>{
           const stationCoords = L.latLng(element.lat, element.lon);
     
-            // Kreiraj novu rutu do benzinske pumpe
+            // Kreiraj novu rutu do prenocista
             L.Routing.control({
               waypoints: [
-                routeCoords[0], // Početna tačka (prva iz postojeće rute)
-                stationCoords   // Benzinska pumpa
+                routeCoords[0], // Početna tačka (prva iz postojece rute)
+                stationCoords   // prenociste
               ],
               routeWhileDragging: true
             }).addTo(this.map!);
         });
       });
-      console.log('Filtrirana prenoćišta:', filteredAccommodations);
+      //console.log('Filtrirana prenoćišta:', filteredAccommodations);
     });
   }
   showAccmmodationsOnRouteV2(routeCoords:L.LatLng[],display:boolean){
@@ -251,9 +305,9 @@ export class PersonalTravelViewComponent implements OnInit{
       const radius=15000;
       const accomodationIcon=L.icon({
         iconUrl: '../../assets/images/hotel.png', // Putanja do ikone prenoćišta
-        iconSize: [16, 16], // Prilagodi veličinu po potrebi
-        iconAnchor: [8, 8], // Podesi tačku sidrišta
-        popupAnchor: [0, -8] // Podesi tačku za prikaz popup-a
+        iconSize: [16, 16], 
+        iconAnchor: [8, 8], 
+        popupAnchor: [0, -8] 
       });
       
       const filteredAccommodations=data.elements.filter((element:any)=>{
@@ -270,9 +324,9 @@ export class PersonalTravelViewComponent implements OnInit{
 
       filteredAccommodations.forEach((element:any) => {
         const accommodationMarker=L.marker([element.lat,element.lon],{icon:accomodationIcon}).addTo(this.map!);
-        accommodationMarker.bindPopup('prenociste');
+        accommodationMarker.bindPopup('accommodation');
       });
-      console.log('Filtrirana prenoćišta:', filteredAccommodations);
+      //console.log('Filtrirana prenoćišta:', filteredAccommodations);
     
     });
   }
@@ -283,9 +337,9 @@ export class PersonalTravelViewComponent implements OnInit{
   
         const restaurantIcon = L.icon({
           iconUrl: '../../assets/images/cutlery.png', // Putanja do ikone restorana
-          iconSize: [16, 16], // Prilagodi veličinu po potrebi
-          iconAnchor: [8, 8], // Podesi tačku sidrišta
-          popupAnchor: [0, -8] // Podesi tačku za prikaz popup-a
+          iconSize: [16, 16], 
+          iconAnchor: [8, 8], 
+          popupAnchor: [0, -8] 
         });
   
         const filteredRestaurants = data.elements.filter((element: any) => {
@@ -301,22 +355,22 @@ export class PersonalTravelViewComponent implements OnInit{
   
         filteredRestaurants.forEach((element: any) => {
           const restaurantMarker = L.marker([element.lat, element.lon], { icon: restaurantIcon }).addTo(this.map!);
-          restaurantMarker.bindPopup('Restoran');
+          restaurantMarker.bindPopup('Restaurant');
           restaurantMarker.on('click',()=>{
             const stationCoords = L.latLng(element.lat, element.lon);
     
-            // Kreiraj novu rutu do benzinske pumpe
+            // Kreiraj novu rutu do restorana
             L.Routing.control({
               waypoints: [
-                routeCoords[0], // Početna tačka (prva iz postojeće rute)
-                stationCoords   // Benzinska pumpa
+                routeCoords[0], // Početna tačka (prva iz postojece rute)
+                stationCoords   //restoran
               ],
               routeWhileDragging: true
             }).addTo(this.map!);
-          });
+          })
         });
   
-        console.log('Filtrirani restorani:', filteredRestaurants);
+       // console.log('Filtrirani restorani:', filteredRestaurants);
       });
   }
   async showRestaurantsOnRouteV2(routeCoords: L.LatLng[],display:boolean,iteration:number) {
@@ -350,7 +404,7 @@ export class PersonalTravelViewComponent implements OnInit{
         if(display){
         filteredRestaurants.forEach((element: any) => {
           const restaurantMarker = L.marker([element.lat, element.lon], { icon: restaurantIcon }).addTo(this.map!);
-          restaurantMarker.bindPopup('Restoran');
+          restaurantMarker.bindPopup('Restaurant');
         });
       }
       else{
@@ -403,7 +457,7 @@ export class PersonalTravelViewComponent implements OnInit{
   
         filteredStations.forEach((element: any) => {
           const chargingMarker = L.marker([element.lat, element.lon], { icon: chargingStationIcon }).addTo(this.map!);
-          chargingMarker.bindPopup('Punjač za električne automobile');
+          chargingMarker.bindPopup('Charging station');
           chargingMarker.on('click',()=>{
             const stationCoords = L.latLng(element.lat, element.lon);
     
@@ -415,10 +469,9 @@ export class PersonalTravelViewComponent implements OnInit{
               ],
               routeWhileDragging: true
             }).addTo(this.map!);
-            
-          });
+          })
         });
-        
+  
         console.log('Filtrirani punjači:', filteredStations);
       });
   }
@@ -448,8 +501,7 @@ export class PersonalTravelViewComponent implements OnInit{
   
         filteredRestAreas.forEach((element: any) => {
           const restAreaMarker = L.marker([element.lat, element.lon], { icon: restAreaIcon }).addTo(this.map!);
-          restAreaMarker.bindPopup('Odmorište');
-          
+          restAreaMarker.bindPopup('Rest area');
         });
   
         console.log('Filtrirana odmorišta:', filteredRestAreas);
@@ -494,7 +546,7 @@ export class PersonalTravelViewComponent implements OnInit{
       const intervalCoords=routeCoords[index];
       //Prikazi odmorista u blizini intervalne tacke
       let coordsArr:L.LatLng[]=[start,intervalCoords];
-      console.log('redni broj iteracije',i);
+      //console.log('redni broj iteracije',i);
       await this.getPOIsOnRouteV2(coordsArr,display,i);
       //azuriraj vreme za sledeci interval sa dodatkom pauze
       currentIntervalTime+=drivingIntervalInSeconds+breakTimeInSeconds;
@@ -531,6 +583,151 @@ export class PersonalTravelViewComponent implements OnInit{
       });
     }
     this.calculateRoute(this.selectedOption);
+  }
+  
+  checkIfPointInCountry(lat:number,lng:number,countryGeoJson:any):boolean{
+    const point=turf.point([lng,lat]);
+    const polygon=turf.polygon(countryGeoJson.features[0].geometry.coordinates);
+    return turf.booleanPointInPolygon(point,polygon);
+  }
+
+  checkRouteInCountry(routeCoords:L.LatLng[],countryGeoJson:any):boolean{
+    return routeCoords.some(coord=>
+      this.checkIfPointInCountry(coord.lat,coord.lng,countryGeoJson)
+    );
+  }
+
+  async loadBulgariaJson(): Promise<any> {
+    try {
+      const response = await fetch('../../assets/geojson/bulgaria.json');
+      this.bulgariaGeoJson = await response.json();
+      
+    } catch (error) {
+      console.error('Greška prilikom učitavanja GeoJSON:', error);
+    }
+  }
+
+  async loadHungaryJson():Promise<any>{
+    try {
+      const response = await fetch('../../assets/geojson/hungary.json');
+      this.hungaryGeoJson = await response.json();
+     
+    } catch (error) {
+      console.error('Greška prilikom učitavanja GeoJSON:', error);
+    }
+  }
+
+  async loadCroatiaJson():Promise<any>
+  {
+    try {
+      const response = await fetch('../../assets/geojson/croatia.json');
+      this.croatiaGeoJson = await response.json();
+      
+    } catch (error) {
+      console.error('Greška prilikom učitavanja GeoJSON:', error);
+    }
+  }
+  
+  async loadBosniaJson():Promise<any>
+  {
+    try {
+      const response = await fetch('../../assets/geojson/bosnia.json');
+      this.bosniaGeoJson = await response.json();
+      
+    } catch (error) {
+      console.error('Greška prilikom učitavanja GeoJSON:', error);
+    }
+  }
+
+  async loadMontenegroJson():Promise<any>
+  {
+    try {
+      const response = await fetch('../../assets/geojson/montenegro.json');
+      this.montenegroGeoJson = await response.json();
+     
+    } catch (error) {
+      console.error('Greška prilikom učitavanja GeoJSON:', error);
+    }
+  }
+  async loadMacedoniaJson():Promise<any>
+  {
+    try {
+      const response = await fetch('../../assets/geojson/macedonia.json');
+      this.macedoniaGeoJson = await response.json();
+      
+    } catch (error) {
+      console.error('Greška prilikom učitavanja GeoJSON:', error);
+    }
+  }
+  async loadRumunyJson():Promise<any>
+  {
+    try {
+      const response = await fetch('../../assets/geojson/rumuny.json');
+      this.rumunyGeoJson = await response.json();
+    } catch (error) {
+      console.error('Greška prilikom učitavanja GeoJSON:', error);
+    }
+  }
+  
+  openCala()
+  {
+    this.dialog.open(CameraCalaComponent);
+  }
+  openKelebija()
+  {
+    this.dialog.open(CameraKelebijaComponent);
+  }
+  openHorgos()
+  {
+    this.dialog.open(CameraHorgosComponent);
+  }
+  openJabuka()
+  {
+    this.dialog.open(CameraJabukaComponent);
+  }
+  openGostun()
+  {
+    this.dialog.open(CameraGostunComponent);
+  }
+  openBatrovci()
+  {
+    this.dialog.open(CameraBatrovciComponent);
+  }
+  openSid()
+  {
+    this.dialog.open(CameraSidComponent);
+  }
+  openVatin()
+  {
+    this.dialog.open(CameraVatinComponent);
+  }
+  openKotroman()
+  {
+    this.dialog.open(CameraKotromanComponent);
+  }
+  openZvornik()
+  {
+    this.dialog.open(CameraZvornikComponent);
+  }
+  openRaca()
+  {
+    this.dialog.open(CameraRacaComponent);
+  }
+  openTrbusnica()
+  {
+    this.dialog.open(CameraTrbusnicaComponent);
+  }
+  openVrskaCuka()
+  {
+    this.dialog.open(CameraVrskaCukaComponent);
+  }
+  openGradina()
+  {
+    this.dialog.open(CameraGradinaComponent);
+  }
+  openPresevo()
+  {
+    this.dialog.open(CameraPresevoComponent);
   }
 
   ngOnInit(): void {
